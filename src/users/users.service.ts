759fbb477d014a4d2c67dce04fea429d69b8bed0
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-06-03 22:40:23 -0300

    Criando service com CRUD para users

---
+++ src/users/users.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from './users.entity';
+import { Repository } from 'typeorm';
+import * as dotenv from 'dotenv';
+import { updateUserDTO, createUserDTO } from './users.dto';
+dotenv.config();
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(Users)
+    private usersRepo: Repository<Users>,
+  ) {}
+
+  async create(data: Partial<createUserDTO>): Promise<Users> {
+    const user = this.usersRepo.create(data);
+    return this.usersRepo.save(user);
+  }
+
+  async update(id: number, updateData: updateUserDTO): Promise<Users> {
+    const user = await this.usersRepo.findOne({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    Object.assign(user, updateData);
+    await this.usersRepo.update(id, updateData);
+
+    return user;
+  }
+
+  async findAll(): Promise<Users[]> {
+    return this.usersRepo.find();
+  }
+
+  async findOne(id: number): Promise<Users | null> {
+    return this.usersRepo.findOneBy({ id });
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.usersRepo.delete(id);
+  }
+}
