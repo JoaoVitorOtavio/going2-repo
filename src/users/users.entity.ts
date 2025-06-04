89c65de5ce91fd55013de8445101a73073653b2a
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-06-03 22:39:37 -0300

    Criando entity de usuario

---
+++ src/users/users.entity.ts
@@ -0,0 +1,24 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { UserRole } from './users.enums';
+
+@Entity()
+export class Users {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.USER,
+  })
+  role: UserRole;
+}
