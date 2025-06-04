bb2b47f08a2ee01fde24618e65ed65d746c9b842
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-06-04 16:36:36 -0300

    Configurando parte de auth para login e uso do jwt

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { UserDTO } from 'src/users/users.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(user: { userEmail: string; password: string }) {
+    const userOnDb = await this.usersService.findOneByEmail(user.userEmail);
+
+    if (userOnDb && userOnDb.password === user.password) {
+      const result: Partial<UserDTO> = { ...userOnDb };
+
+      delete result.password;
+
+      return {
+        access_token: this.jwtService.sign(result),
+      };
+    }
+
+    throw new UnauthorizedException();
+  }
+}
