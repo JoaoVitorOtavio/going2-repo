89cf0bda58fd08b7e763da8a94e131c86b0a5471
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-06-05 15:23:27 -0300

    Importando casl no modulo de user

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './http/users.controller';
 import { Users } from './users.entity';
+import { CaslModule } from 'src/casl/casl.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [TypeOrmModule.forFeature([Users]), CaslModule],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [TypeOrmModule],
