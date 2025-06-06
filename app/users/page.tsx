e489e40fcb32a4f607533f9dc427564dbad9089b
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-06-06 14:16:25 -0300

    Adicionando delete de user

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,27 +1,46 @@
 "use client";
-import { IUser } from "@/commons/interfaces/users";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteUser, setUsers } from "../../store/slices/userSlice";
+import { RootState } from "../../store/store";
 import { userService } from "@/services/userService";
 import { redirect } from "next/navigation";
-import { useEffect, useState } from "react";
+import LoadingSpinner from "../components/Spinner/Spinner";
+import { IUser } from "@/commons/interfaces/users";
 
 export default function Users() {
-  const [users, setUsers] = useState<IUser[]>([]);
+  const dispatch = useDispatch();
+  const users = useSelector((state: RootState) => state.user.users);
+
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    async function getAllUsers() {
+    async function fetchUsers() {
       const result = await userService.findAll();
-      setUsers(result);
+      dispatch(setUsers(result));
+      setIsLoading(false);
     }
 
-    getAllUsers();
-  }, []);
+    fetchUsers();
+  }, [dispatch]);
+
+  const handleDelete = async (id: number) => {
+    try {
+      await userService.delete(id);
+      dispatch(deleteUser(id));
+    } catch (err) {
+      console.error("Erro ao deletar usuário:", err);
+    }
+  };
+
+  if (isLoading) return <LoadingSpinner />;
 
   return (
     <div className="min-h-screen bg-gray-100 p-6">
       <h1 className="text-3xl font-bold text-center mb-8">Usuários</h1>
 
       <div className="space-y-4 max-w-4xl mx-auto">
-        {users.map((user) => (
+        {users.map((user: IUser) => (
           <div
             key={user.id}
             className="bg-white shadow rounded-md p-4 flex flex-col md:flex-row md:items-center md:justify-between gap-4"
@@ -44,29 +63,19 @@ export default function Users() {
             <div className="flex gap-2">
               <button
                 onClick={() => redirect(`/update/user/${user.id}`)}
-                className="px-4 py-1 bg-yellow-500 text-white rounded hover:bg-yellow-600"
+                className="cursor-pointer px-4 py-1 bg-yellow-500 text-white rounded hover:bg-yellow-600"
               >
                 Editar
               </button>
               <button
-                onClick={() => userService.delete(user.id)}
-                className="px-4 py-1 bg-red-600 text-white rounded hover:bg-red-700"
+                onClick={() => handleDelete(user.id)}
+                className="cursor-pointer px-4 py-1 bg-red-600 text-white rounded hover:bg-red-700"
               >
                 Deletar
               </button>
             </div>
           </div>
         ))}
-      </div>
-
-      <div className="mt-8 text-center">
-        <button
-          type="button"
-          onClick={() => redirect("/")}
-          className="bg-blue-600 text-white py-2 px-6 rounded hover:bg-blue-700"
-        >
-          Voltar para Home
-        </button>
       </div>
     </div>
   );
