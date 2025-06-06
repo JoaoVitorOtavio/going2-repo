771293c4343affbd919f41a49d0cde8aa50fc130
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-06-05 23:04:31 -0300

    Adicionando funco de update e findOne em userService

--- services/userService.ts
+++ services/userService.ts
@@ -3,6 +3,27 @@ import { IUser } from "@/commons/interfaces/users";
 import { toast } from "react-toastify";
 
 export const userService = {
+  async findOne(userId: number): Promise<IUser | null> {
+    const token = localStorage.getItem("token");
+
+    const res = await fetch(`${API_URL}/users/${userId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!res.ok) {
+      const error = await res.json();
+      toast.error("Erro ao buscar usuario!");
+      throw new Error(error.message || "Erro ao buscar usuario!");
+    }
+
+    const result: IUser = await res.json();
+
+    return result;
+  },
   async findAll(): Promise<IUser[]> {
     const token = localStorage.getItem("token");
 
@@ -24,12 +45,49 @@ export const userService = {
 
     return result;
   },
+  async update(userId: number, user: Partial<IUser>): Promise<IUser> {
+    try {
+      const token = localStorage.getItem("token");
+
+      const res = await fetch(`${API_URL}/users/${userId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(user),
+      });
+
+      if (!res.ok) {
+        const result = await res.json();
+
+        toast.error(result.message || "Erro ao atualizar usuário");
+        throw new Error(result.message || "Erro ao atualizar usuário");
+      }
+
+      const result: IUser = await res.json();
+
+      toast.success("Usuario atualizado com sucesso!");
+      return result;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+        throw new Error(error.message || "Erro ao atualizar usuario!");
+      } else {
+        toast.error("Erro ao atualizar usuario!");
+        throw new Error("Erro ao atualizar usuario!");
+      }
+    }
+  },
   async create(user: Partial<IUser>): Promise<IUser> {
     try {
+      const token = localStorage.getItem("token");
+
       const res = await fetch(`${API_URL}/users`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify(user),
       });
