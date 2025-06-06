c68714ab3aa02a5ecf856cb558599f917b953d5f
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-06-05 23:14:45 -0300

    Adicionando tratamento de erro no service de user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import {
   BadRequestException,
   Injectable,
+  InternalServerErrorException,
   NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -35,31 +36,99 @@ export class UsersService {
   }
 
   async update(id: number, updateData: updateUserDTO): Promise<Users> {
-    const user = await this.usersRepo.findOne({ where: { id } });
+    try {
+      const user = await this.usersRepo.findOne({ where: { id } });
 
-    if (!user) {
-      throw new NotFoundException('User not found');
+      if (!user) {
+        throw new NotFoundException('Usuário não encontrado');
+      }
+
+      Object.assign(user, updateData);
+      await this.usersRepo.update(id, updateData);
+
+      return user;
+    } catch (error: any) {
+      if (error instanceof Error) {
+        throw new BadRequestException(
+          error?.message || 'Erro ao criar usuário',
+        );
+      }
+
+      throw new BadRequestException('Erro ao atualizar usuário');
     }
-
-    Object.assign(user, updateData);
-    await this.usersRepo.update(id, updateData);
-
-    return user;
   }
 
   async findAll(): Promise<Users[]> {
-    return this.usersRepo.find();
+    try {
+      return await this.usersRepo.find();
+    } catch (error: any) {
+      if (error instanceof Error) {
+        throw new InternalServerErrorException(
+          error?.message || 'Erro ao buscar usuários',
+        );
+      }
+
+      throw new InternalServerErrorException('Erro ao buscar usuários');
+    }
   }
 
   async findOne(id: number): Promise<Users | null> {
-    return this.usersRepo.findOneBy({ id });
+    try {
+      const user = await this.usersRepo.findOneBy({ id });
+
+      if (!user) {
+        throw new NotFoundException('Usuário não encontrado');
+      }
+
+      return user;
+    } catch (error: any) {
+      if (error instanceof Error) {
+        throw new BadRequestException(
+          error?.message || 'Erro ao buscar usuário',
+        );
+      }
+
+      throw new BadRequestException('Erro ao buscar usuário');
+    }
   }
 
   async findOneByEmail(email: string): Promise<Users | null> {
-    return this.usersRepo.findOneBy({ email });
+    try {
+      const user = await this.usersRepo.findOneBy({ email });
+
+      if (!user) {
+        throw new NotFoundException('Usuário não encontrado');
+      }
+
+      return user;
+    } catch (error: any) {
+      if (error instanceof Error) {
+        throw new BadRequestException(
+          error?.message || 'Erro ao buscar usuário por e-mail',
+        );
+      }
+
+      throw new BadRequestException('Erro ao buscar usuário por e-mail');
+    }
   }
 
   async remove(id: number): Promise<void> {
-    await this.usersRepo.delete(id);
+    try {
+      const user = await this.usersRepo.findOneBy({ id });
+
+      if (!user) {
+        throw new NotFoundException('Usuário não encontrado');
+      }
+
+      await this.usersRepo.delete(id);
+    } catch (error: any) {
+      if (error instanceof Error) {
+        throw new BadRequestException(
+          error?.message || 'Erro ao deletar usuário',
+        );
+      }
+
+      throw new BadRequestException('Erro ao deletar usuário');
+    }
   }
 }
