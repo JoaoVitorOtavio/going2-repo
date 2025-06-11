dbca46a01927a39ca0c6a25c070257891c2351a4
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-06-11 13:14:10 -0300

    Arrumando estrutura dos teste pois nao estava sendo injetado de forma correta o service Adicionando teste para erros na parte do remove

--- going2-back/src/users/tests/user.service.spec.ts
+++ going2-back/src/users/tests/user.service.spec.ts
@@ -1,75 +1,95 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { UsersService } from '../users.service';
 import { Users } from '../users.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import {
+  BadRequestException,
   InternalServerErrorException,
   NotFoundException,
 } from '@nestjs/common';
+import { UsersController } from '../http/users.controller';
+import { AbilityFactory } from 'src/casl/casl-ability.factory/casl-ability.factory';
 
-let userService: UsersService;
+let usersService: UsersService;
 
-beforeAll(async () => {
-  const mockUsersRepo = {
-    find: jest.fn().mockResolvedValue([]),
-    findOne: jest.fn(),
-    save: jest.fn(),
-  };
+const mockUsersRepo = {
+  find: jest.fn(),
+  findOneBy: jest.fn(),
+  save: jest.fn(),
+  delete: jest.fn(),
+} as Record<string, jest.Mock>;
 
-  const app: TestingModule = await Test.createTestingModule({
+beforeEach(async () => {
+  const moduleRef = await Test.createTestingModule({
+    controllers: [UsersController],
     providers: [
       UsersService,
       {
         provide: getRepositoryToken(Users),
         useValue: mockUsersRepo,
       },
+      {
+        provide: AbilityFactory,
+        useValue: {
+          defineAbility: jest.fn(),
+        },
+      },
     ],
   }).compile();
 
-  userService = app.get<UsersService>(UsersService);
+  usersService = moduleRef.get(UsersService);
 });
 
 describe('UserService', () => {
   it('Should be defined', () => {
-    expect(userService).toBeDefined();
+    expect(usersService).toBeDefined();
   });
 
   it('Should find All Users', async () => {
-    userService.findAll = jest.fn().mockResolvedValueOnce({});
+    mockUsersRepo.find.mockResolvedValueOnce([]);
 
-    const result = await userService.findAll();
-    expect(result).toEqual({});
+    const result = await usersService.findAll();
+    expect(result).toEqual([]);
   });
 
   it('Should throw InternalServerErrorException when there is an error on findAll', async () => {
-    userService.findAll = jest
-      .fn()
-      .mockRejectedValueOnce(
-        new InternalServerErrorException('Erro ao buscar usuários'),
-      );
+    mockUsersRepo.find.mockRejectedValueOnce(
+      new InternalServerErrorException('Erro ao buscar usuários'),
+    );
 
-    await expect(userService.findAll()).rejects.toThrow(
+    await expect(usersService.findAll()).rejects.toThrow(
       InternalServerErrorException,
     );
   });
 
-  it('Should remove an user', async () => {
-    const removeMock = jest
-      .spyOn(userService, 'remove')
-      .mockResolvedValue(undefined);
+  it("Should return notFoundException when user doesn't exist", async () => {
+    mockUsersRepo.findOneBy.mockResolvedValueOnce(undefined);
 
-    const result = await userService.remove(1);
-
-    expect(result).toEqual(undefined);
-    expect(removeMock).toHaveBeenCalledWith(1);
+    await expect(usersService.remove(1)).rejects.toThrow(NotFoundException);
+    expect(mockUsersRepo.findOneBy).toHaveBeenCalledWith({ id: 1 });
+    expect(mockUsersRepo.delete).not.toHaveBeenCalled();
   });
 
-  it("Should return notFoundException when user doesn't exist", async () => {
-    const removeMock = jest
-      .spyOn(userService, 'remove')
-      .mockRejectedValueOnce(new NotFoundException('Usuário não encontrado'));
+  it('Should return badRequestException when there is an error on remove', async () => {
+    mockUsersRepo.findOneBy.mockRejectedValueOnce(new BadRequestException());
 
-    await expect(userService.remove(1)).rejects.toThrow(NotFoundException);
-    expect(removeMock).toHaveBeenCalledWith(1);
+    await expect(usersService.remove(1)).rejects.toThrow(BadRequestException);
+    expect(mockUsersRepo.findOneBy).toHaveBeenCalledWith({ id: 1 });
+    expect(mockUsersRepo.delete).not.toHaveBeenCalled();
+  });
+
+  it('Should remove an user', async () => {
+    mockUsersRepo.findOneBy.mockResolvedValueOnce({
+      id: 1,
+      name: 'mockedUser',
+    });
+
+    mockUsersRepo.delete.mockResolvedValueOnce(undefined);
+
+    const result = await usersService.remove(1);
+
+    expect(mockUsersRepo.findOneBy).toHaveBeenCalledWith({ id: 1 });
+    expect(mockUsersRepo.delete).toHaveBeenCalledWith(1);
+    expect(result).toBeUndefined();
   });
 });
